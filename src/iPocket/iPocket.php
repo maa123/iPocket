182eeb0056c1dc5d528183bee655ca4e74995f09
maa123 <maa123.lq@gmail.com>
2016-01-13 18:30:17 +0900

    Update iPocket.php

--- src/iPocket/iPocket.php
+++ src/iPocket/iPocket.php
@@ -5,9 +5,21 @@ namespace {
 }
 
 namespace iPocket{
-
+    	const VERSION = "1.0dev";
+	const API_VERSION = "0.13.1";
+	const CODENAME = "[Ipocket]";
+	const MINECRAFT_VERSION = "v0.13.1 alpha";
+	const MINECRAFT_VERSION_NETWORK = "0.13.1";
+	const IPOCKET_HOST_SERVER_URL = "http://api.ipocket.link";
+	set_time_limit(0);//勝手に終了されないように
+    	ini_set("allow_url_fopen", 1);//インターネットに接続
+	ini_set("display_errors", 1);
+	ini_set("display_startup_errors", 1);
+	ini_set("default_charset", "utf-8");//文字コードはUTF-8
 	echo "Testing...\n";
-
+    	function RunServer(){
+    	
+    	}
 
 
 	
